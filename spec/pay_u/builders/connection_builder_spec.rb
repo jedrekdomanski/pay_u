b4b26809462311b98f09e0c814339cff5bdfdea9
Jędrek Domański <jedrek.domanski@gmail.com>
2019-12-25 12:34:36 +0100

    Update connection spec

--- spec/pay_u/builders/connection_builder_spec.rb
+++ spec/pay_u/builders/connection_builder_spec.rb
@@ -1,7 +1,12 @@
 RSpec.describe PayU::ConnectionBuilder, :unit do
   let(:faraday) { spy(:faraday) }
+  let(:connection) { spy(:connection) }
+
+  subject { described_class.new(faraday: faraday) }
 
   it 'returns a Faraday connection' do
+    allow(faraday).to receive(:new).and_yield(connection)
+
     connection = described_class.call
 
     expect(connection).to be_a(Faraday::Connection)
