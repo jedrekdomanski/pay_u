09a80fc70078c997f68305963371c56d98e3f43d
Jędrek Domański <jedrek.domanski@gmail.com>
2019-12-29 23:17:58 +0100

    Add CancelOrder service

--- lib/pay_u/requests/cancel_order.rb
+++ lib/pay_u/requests/cancel_order.rb
@@ -1,16 +1,21 @@
 module PayU
   module Requests
     class CancelOrder < Service
-      def initialize(connection_builder: ConnectionBuilder)
+      def initialize(
+        connection_builder: ConnectionBuilder,
+        authorize_request: Authorize
+      )
         @connection_builder = connection_builder
+        @authorize_request  = authorize_request
       end
 
-      def call(token:, order_id:)
+      def call(order_id:)
+        auth_response = @authorize_request.call
         connection = @connection_builder.call
 
         url = build_url(order_id)
         connection.delete(url) do |request|
-          request.headers['Authorization'] = "Bearer #{token}"
+          request.headers['Authorization'] = "Bearer #{auth_response.token}"
         end
       end
 
--- spec/pay_u/requests/cancel_order_spec.rb
+++ spec/pay_u/requests/cancel_order_spec.rb
@@ -1,10 +1,49 @@
 RSpec.describe PayU::Requests::CancelOrder, :unit do
   let(:connection_builder) { spy(:connection_builder) }
-
-  subject do
-    described_class.new(connection_builder: connection_builder)
+  let(:authorize_request) { spy(:authorize_request) }
+  let(:connection) { double(:connection) }
+  let(:auth_response) do
+    double(
+      :auth_response,
+      status: 200,
+      message: 'OK',
+      token: '123sda12e'
+    )
   end
 
-  it 'cancels order if authorized'
-  it 'raises 401 if unauthorized'
+  subject do
+    described_class.new(
+      connection_builder: connection_builder,
+      authorize_request: authorize_request
+    )
+  end
+
+  it 'cancels order' do
+    order_id = '123asdasda123'
+
+    response = double(
+      :response,
+      body: {
+        'orderId' => '67RDG4GV4Z191228GUEST000P01',
+        'status' => {
+          'statusCode' => 'SUCCESS',
+          'statusDesc' => 'Request processing successful'
+        }
+      },
+      status: 200,
+      reason_phrase: 'OK'
+    )
+
+    allow(authorize_request).to receive(:call).and_return(auth_response)
+    allow(connection_builder).to receive(:call).and_return(connection)
+    allow(connection)
+      .to receive(:delete)
+      .with("https://secure.snd.payu.com/api/v2_1/orders/#{order_id}")
+      .and_return(response)
+
+    result = subject.call(order_id: order_id)
+
+    expect(result.status).to eq(200)
+    expect(result.body['status']['statusDesc']).to eq('Request processing successful')
+  end
 end
