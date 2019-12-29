e17ae13b2647cf8c47ddaf8ba062ab48e5b365a2
Jędrek Domański <jedrek.domanski@gmail.com>
2019-12-29 01:55:49 +0100

    Add GetOrder service

--- lib/pay_u/requests/get_order.rb
+++ lib/pay_u/requests/get_order.rb
@@ -1,17 +1,24 @@
 module PayU
   module Requests
     class GetOrder < Service
-      def initialize(connection_builder: ConnectionBuilder)
+      def initialize(
+        connection_builder: ConnectionBuilder,
+        authorize_request: Authorize
+      )
         @connection_builder = connection_builder
+        @authorize_request  = authorize_request
       end
 
-      def call(token:, order_id:)
+      def call(order_id:)
+        auth_response = @authorize_request.call
         connection = @connection_builder.call
-
         url = build_url(order_id)
-        connection.get(url) do |request|
-          request.headers['Authorization'] = "Bearer #{token}"
+
+        response = connection.get(url) do |request|
+          request.headers['Authorization'] = "Bearer #{auth_response.token}"
         end
+
+        build_response(response)
       end
 
       private
@@ -20,6 +27,40 @@ module PayU
         PayU.configuration.base_url \
           + Requests::ORDERS_URL \
           + "/#{order_id}"
+      end
+
+      def build_response(response)
+        case response.status
+        when 200 then build_order_found(response)
+        when 404 then build_order_not_found(response)
+        end
+      end
+
+      def build_order_found(response)
+        order = response.body['orders'].first
+
+        Response::OrderFound.new(
+          order_id: order['orderId'],
+          order_created_date: order['orderCreateDate'],
+          notify_url: order['notifyUrl'],
+          customer_ip: order['customerIp'],
+          merchant_pos_id: order['merchantPosId'],
+          description: order['description'],
+          currency_code: order['currencyCode'],
+          total_amount: order['totalAmount'],
+          status: response.body['status']['statusDesc'],
+          products: order['products']
+        )
+      end
+
+      def build_order_not_found(response)
+        data = response.body['status']
+
+        Response::OrderNotFound.new(
+          status: data['statusCode'],
+          severity: data['severity'],
+          description: data['statusDesc']
+        )
       end
     end
   end
--- spec/pay_u/requests/get_order_spec.rb
+++ spec/pay_u/requests/get_order_spec.rb
@@ -1,10 +1,102 @@
 RSpec.describe PayU::Requests::GetOrder, :unit do
   let(:connection_builder) { spy(:connection_builder) }
-
+  let(:authorize_request) { spy(:authorize_request) }
+  let(:connection) { double(:connection) }
+  let(:auth_response) do
+    double(
+      :auth_response,
+      status: 200,
+      message: 'OK',
+      token: '123sda12e'
+    )
+  end
   subject do
-    described_class.new(connection_builder: connection_builder)
+    described_class.new(
+      connection_builder: connection_builder,
+      authorize_request: authorize_request
+    )
   end
 
-  it 'fetches order info if authorized'
-  it 'raises 401 if unauthorized'
+  it 'returns order info if order found' do
+    response = double(
+      :response,
+      body: {
+        'orders' => [
+          {
+            'orderId' => '67RDG4GV4Z191228GUEST000P01',
+            'orderCreateDate' => '2019-12-28T23:28:40.129+01:00',
+            'notifyUrl' => 'http://localhost:3000',
+            'customerIp' => '127.0.0.1',
+            'merchantPosId' => '370379',
+            'description' => 'RTV market',
+            'currencyCode' => 'PLN',
+            'totalAmount' => '21000',
+            'status' => 'NEW',
+            'products' => [
+              {
+                'name' => 'Wireless Mouse for Laptop',
+                'unitPrice' => '15000',
+                'quantity' => '1'
+              },
+              {
+                'name' => 'HDMI cable',
+                'unitPrice' => '6000',
+                'quantity' => '1'
+              }
+            ]
+          }
+        ],
+        'status' => {
+          'statusCode' => 'SUCCESS',
+          'statusDesc' => 'Request processing successful'
+        }
+      },
+      status: 404
+    )
+    allow(authorize_request)
+      .to receive(:call).and_return(auth_response)
+    allow(connection_builder).to receive(:call).and_return(connection)
+    allow(connection)
+      .to receive(:get)
+      .with('https://secure.snd.payu.com/api/v2_1/orders/67RDG4GV4Z191228GUEST000P01')
+      .and_return(response)
+    allow(response)
+      .to receive(:status)
+      .and_return(200)
+
+    result = subject.call(order_id: '67RDG4GV4Z191228GUEST000P01')
+
+    expect(result.status).to eq('Request processing successful')
+    expect(result.order_id).to eq('67RDG4GV4Z191228GUEST000P01')
+  end
+  it 'raises 404 if order not found' do
+    response = double(
+      :response,
+      body: {
+        'status' => {
+          'statusCode' => 'DATA_NOT_FOUND',
+          'severity' => 'INFO',
+          'statusDesc' => 'Could not find data for given criteria.'
+        }
+      },
+      status: 404
+    )
+
+    allow(authorize_request)
+      .to receive(:call).and_return(auth_response)
+    allow(connection_builder).to receive(:call).and_return(connection)
+    allow(connection)
+      .to receive(:get)
+      .with('https://secure.snd.payu.com/api/v2_1/orders/non_existent_order_id')
+      .and_return(response)
+    allow(response)
+      .to receive(:status)
+      .and_return(404)
+
+    result = subject.call(order_id: 'non_existent_order_id')
+
+    expect(result.status).to eq('DATA_NOT_FOUND')
+    expect(result.severity).to eq('INFO')
+    expect(result.description).to eq('Could not find data for given criteria.')
+  end
 end
