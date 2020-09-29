b659be1418074f30eae4b4cc6afc8bdfabb1078c
jedrekdomanski <jedrek.domanski@gmail.com>
2020-09-29 23:59:42 +0200

    Try to fix tests

--- spec/pay_u/requests/payment_methods_spec.rb
+++ spec/pay_u/requests/payment_methods_spec.rb
@@ -1,6 +1,14 @@
+# frozen_string_literal: true
+
 RSpec.describe PayU::Requests::PaymentMethods, :unit do
   it 'returns payment methods' do
+    token = double(:token)
+    response = double(:response, token: token)
+
     response = VCR.use_cassette('requests/pay_methods') do
+      expect(PayU::Requests::Authorize).to receive(:call)
+        .and_return(response)
+
       described_class.call
     end
 
