77f9d7f5582bcb281e32da35f5382d4b82b389b1
jedrekdomanski <jedrek.domanski@gmail.com>
2020-09-30 23:52:08 +0200

    add tests and rspec config (#9)

--- spec/pay_u/requests/payment_methods_spec.rb
+++ spec/pay_u/requests/payment_methods_spec.rb
@@ -1,6 +1,27 @@
 # frozen_string_literal: true
 
 RSpec.describe PayU::Requests::PaymentMethods, :unit do
-  it 'does something' do
+  let(:connection_builder) { spy(:connection_builder) }
+  let(:authorize_request) { spy(:authorize_request) }
+
+  subject do
+    described_class.new(
+      connection_builder: connection_builder,
+      authorize_request: authorize_request
+    )
+  end
+
+  it 'returns payment methods' do
+    connection = double(:connection)
+    auth_response = double(:auth_response)
+    response = double(:response, body: [])
+
+    allow(connection_builder).to receive(:call).and_return(connection)
+    allow(connection).to receive(:get)
+      .with('https://secure.snd.payu.com/api/v2_1/paymethods')
+      .and_return(response)
+    allow(authorize_request).to receive(:call).and_return(auth_response)
+
+    subject.call
   end
 end
